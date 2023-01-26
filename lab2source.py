48d86b2c8fa8674a3039b80cf98c40489fc9123c
tomhorizon <60243018+tomhorizon@users.noreply.github.com>
2023-01-26 13:33:54 -0800

    Update lab2source.py
    added more

--- lab2source.py
+++ lab2source.py
@@ -2,3 +2,13 @@
 
 pinB6 = pyb.Pin (pyb.Pin.board.PB6, pyb.Pin.IN)
 pinB7 = pyb.Pin (pyb.Pin.board.PB7, pyb.Pin.IN)
+
+timer = pyb.Timer(4, prescalar = 0, period = 0xFFFF)
+
+ch1 = timer.channel(1, pyb.Timer.ENC_A, pin = pinB6)
+ch2 = timer.channel(2, pyb.Timer.ENC_B, pin = pinB7)
+
+while True:
+    count1 = ch1.counter()
+    count2 = ch2.counter()
+    print(count1 + "\t" + count2)
