4acacce27ea45421604a946a254623be34eff8ed
tomhorizon <60243018+tomhorizon@users.noreply.github.com>
2023-01-26 13:43:45 -0800

    encoder encodes
    encoder encodes

--- lab2source.py
+++ lab2source.py
@@ -1,14 +1,20 @@
 # code
 
+import utime
+
 pinB6 = pyb.Pin (pyb.Pin.board.PB6, pyb.Pin.IN)
 pinB7 = pyb.Pin (pyb.Pin.board.PB7, pyb.Pin.IN)
 
-timer = pyb.Timer(4, prescalar = 0, period = 0xFFFF)
+timer = pyb.Timer(4, prescaler = 0, period = 0xFFFF)
 
-ch1 = timer.channel(1, pyb.Timer.ENC_A, pin = pinB6)
-ch2 = timer.channel(2, pyb.Timer.ENC_B, pin = pinB7)
+ch1 = timer.channel(1, pyb.Timer.ENC_AB, pin = pinB6)
+ch2 = timer.channel(2, pyb.Timer.ENC_AB, pin = pinB7)
+
+count_old = 0
 
 while True:
-    count1 = ch1.counter()
-    count2 = ch2.counter()
-    print(count1 + "\t" + count2)
+    count = timer.counter()
+    if count_old != count:
+       print("Count 1: " + str(count))
+        
+    count_old = count
